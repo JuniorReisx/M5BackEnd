62cf4187b2f92adc578a7236ba6fb79fc523a674
JuniorReisx <gilmaikjunior@gmail.com>
2024-10-07 21:16:53 -0300

    ♻️ refactor: rename model files for better structure

---
+++ src/models/job.model.js
@@ -0,0 +1,15 @@
+import { database } from "../database/db.js";
+import { DataTypes } from "sequelize";
+
+const Job = database.define('Job', {
+  nome: { type: DataTypes.STRING, allowNull: false },
+  cargo: { type: DataTypes.STRING, allowNull: false },
+  nivel: { type: DataTypes.STRING, allowNull: false },
+  empresa: { type: DataTypes.STRING, allowNull: false },
+  quantidade: { type: DataTypes.INTEGER, allowNull: false },
+  salario: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
+  localizacao: { type: DataTypes.STRING, allowNull: false },
+  descricao: { type: DataTypes.TEXT, allowNull: false }
+});
+
+export { Job }
