b4146bd9d3ad71942687ae41a66f8112ad456d55
JuniorReisx <gilmaikjunior@gmail.com>
2024-10-07 21:13:33 -0300

    🗑️ chore: delete unnecessary files

--- src/routes/jobRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { listJobs, createJob, deleteJob } = require('../controllers/jobController');
-const router = express.Router();
-
-router.get('/jobs', listJobs);
-router.post('/jobs', createJob);
-router.delete('/jobs/:id', deleteJob);
-
-module.exports = router;
