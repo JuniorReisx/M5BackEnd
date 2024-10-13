470aa136b8dce6feb026b27c04e4904eb78c0d1a
Junior Reis <gilmaik2010@gmail.com>
2024-10-13 12:46:41 -0300

    ✨ feat(job): adding update route to job

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { listUsers, createUser, deleteUser } from '../controllers/user.controller.js';
+import { listUsers, createUser, deleteUser, updateUser} from '../controllers/user.controller.js';
 
 const userRoutes = express.Router();
 
 userRoutes.get('/users', listUsers);
 userRoutes.post('/users', createUser);
 userRoutes.delete('/users/:id', deleteUser);
+userRoutes.put('/users/:id', updateUser)
 
 export { userRoutes };
