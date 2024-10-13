1439f54b0d3f5581d832f84aba1a1708778b3cd8
JuniorReisx <gilmaikjunior@gmail.com>
2024-10-13 13:24:33 -0300

    ✨ feat: add routes for management

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { listJobs, createJob, deleteJob } from '../controllers/job.controller.js';
+import { listJobs, createJob, deleteJob, updateJob } from '../controllers/job.controller.js';
 
 const jobRoutes = express.Router();
 
 jobRoutes.get('/jobs', listJobs);
 jobRoutes.post('/jobs', createJob);
 jobRoutes.delete('/jobs/:id', deleteJob);
+jobRoutes.put('/jobs/:id', updateJob)
 
 export { jobRoutes };
--- src/routes/recruiter.routes.js
+++ src/routes/recruiter.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { listRecruiters, createRecruiter, deleteRecruiter } from '../controllers/recruiter.controller.js';
+import { listRecruiters, createRecruiter, deleteRecruiter, updateRecruiter } from '../controllers/recruiter.controller.js';
 
 const recruiterRoutes = express.Router();
 
 recruiterRoutes.get('/recruiters', listRecruiters);
 recruiterRoutes.post('/recruiters', createRecruiter);
 recruiterRoutes.delete('/recruiters/:id', deleteRecruiter);
+recruiterRoutes.put('/recruiters/:id', updateRecruiter)
 
 export { recruiterRoutes };
