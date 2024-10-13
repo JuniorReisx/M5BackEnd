f3c2ca4bd7c93c791cc978bf476b5ad42aa0c756
Junior Reis <gilmaik2010@gmail.com>
2024-10-13 12:46:08 -0300

    ✨ feat(job): adding id to job model

--- src/models/job.model.js
+++ src/models/job.model.js
@@ -2,6 +2,7 @@ import { database } from "../database/db.js";
 import { DataTypes } from "sequelize";
 
 const Job = database.define('Job', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   nome: { type: DataTypes.STRING, allowNull: false },
   cargo: { type: DataTypes.STRING, allowNull: false },
   nivel: { type: DataTypes.STRING, allowNull: false },
