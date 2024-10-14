a61f555282ea4c4e9fca047f21ae5eb78f1fbca2
JuniorReisx <gilmaikjunior@gmail.com>
2024-10-14 11:57:25 -0300

    🔒 chore: update URL in CORS configuration

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const corsOptions = {
-  origin: 'https://m5-front-end-psi.vercel.app', 
+  origin: ['https://m5-front-ot3p5cat7-junior-reis-projects.vercel.app', 'https://m5-front-end-psi.vercel.app'],
   methods: ['GET', 'POST','PUT','DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
